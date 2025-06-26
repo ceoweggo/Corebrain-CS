d18f66715d0bcff2837c54e655c8d023ae7d47d7
DomiJaskula <domcaj07@gmail.com>
2025-06-26 11:03:34 +0200

    Updated the CorebrainCS.Tests to work with the new version

--- CorebrainCS.Tests/Program.cs
+++ CorebrainCS.Tests/Program.cs
@@ -1,8 +1,8 @@
-﻿using CorebrainCS;
+﻿// using CorebrainCS;
 
 Console.WriteLine("Hello, World!");
 
 // For now it only works on windows
 var corebrain = new CorebrainCS.CorebrainCS("../corebrain/venv/Scripts/python.exe", "../corebrain/corebrain/cli", false);
 
-// Console.WriteLine(corebrain.Version());
+// Console.WriteLine(await corebrain.Version());
